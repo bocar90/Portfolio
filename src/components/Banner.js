e4b90b19a447bbe8fdbfb9707b2cab1b57af1688
Bokar Dieng <43213652+bocar90@users.noreply.github.com>
2023-07-28 17:09:56 -0400

    Update Banner.js

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react";
 import { Container, Row, Col } from "react-bootstrap";
-import headerImg from "../assets/img/floating spaceman.gif";
+import headerImg from "../assets/img/capstone.gif";
 import { ArrowRightCircle } from 'react-bootstrap-icons';
 import 'animate.css';
 import TrackVisibility from 'react-on-screen';
